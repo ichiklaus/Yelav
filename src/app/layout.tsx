9750cf059174e0f38df4464d8412fb4fcf89177d
Yeraldo Nicolás Moreira <nicola1994n@gmail.com>
2025-05-20 14:43:00 -0500

    feat: add og and twitter meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,32 @@ export const metadata = {
   title: 'Yeraldo Nicolás Moreira | Web Developer',
   description:
     'Yeraldo Nicolás Moreira is a Web Developer with a degree in Computer Systems Engineering who enjoys creating amazing web products and delivering great user experiences',
+  openGraph: {
+    title: 'Yeraldo Nicolás Moreira | Web Developer',
+    description:
+      'Yeraldo Nicolás Moreira is a Web Developer with a degree in Computer Systems Engineering who enjoys creating amazing web products and delivering great user experiences',
+    url: 'https://yelav.me/',
+    siteName: 'Yeraldo Nicolás Moreira',
+    images: [
+      {
+        url: 'https://yelav.me/og-image.png', // Replace with your OG image URL
+        width: 1200,
+        height: 630,
+        alt: 'Yeraldo Nicolás Moreira Portfolio',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Yeraldo Nicolás Moreira | Web Developer',
+    description:
+      'Yeraldo Nicolás Moreira is a Web Developer with a degree in Computer Systems Engineering who enjoys creating amazing web products and delivering great user experiences',
+    creator: '@ichiklaus', // Optional
+    site: '@ichiklaus', // Optional
+    images: ['https://yelav.me/og-image.png'], // Same or different from OG image
+  },
 };
 
 export default function RootLayout({
@@ -18,7 +44,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={`${inter.className} `}>
+      <body className={`${inter.className}`}>
         <Header />
         {children}
         <Footer />
